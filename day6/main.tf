efefb5f122a42631f06e40020e56a5e83249482c
VishnuVardhan Reddy <155063589+KatamreddyVishnu@users.noreply.github.com>
2024-12-04 19:43:17 +0530

    Update main.tf

--- day6/main.tf
+++ day6/main.tf
@@ -1,1 +1,26 @@
+provider "aws" {
+  access_key = "AKIATCKAOQX2N7747KNT"
+  secret_key = "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"
+  region = "us-east-1"
+}
 
+variable "ami" {
+  description = "value"
+}
+
+variable "instance_type" {
+  description = "value"
+  type = map(string)
+
+  default = {
+    "dev" = "t2.nano"
+    "stage" = "t1.micro"
+    "prod" = "t2.micro"
+  }
+}
+
+module "ec2_instance" {
+  source = "./modules/ec2_instance"
+  ami = var.ami
+  instance_type = lookup(var.instance_type, terraform.workspace, "t2.micro")
+}
